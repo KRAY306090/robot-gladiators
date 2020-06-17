50c3157c7e7a71f59a336a3db8e6d8fc8f702af8
Kennadee <kennadee.ray@gmail.com>
2020-06-17 08:55:50 -0500

    if/else statements added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
@@ -8,6 +9,10 @@ var enemyAttack = 12;
 
 var fight = function () {
     window.alert("Welcome to Robot Gladiators!");
+
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
+
+  if (promptFight === "fight" || promptFight === "FIGHT")  {
 
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
@@ -39,6 +44,21 @@ var fight = function () {
   else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+}
+else if (promptFight === "skip" || promptFight === "SKIP") {
+   var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+   if (confirmSkip) {
+       window.alert(playerName + " has chosen to skip this fight. Goodbye!");
+       playerMoney = playerMoney - 2;
+   }
+   else {
+       fight ();
+   }
+}
+else {
+    window.alert("You need to pick a valid option. Try again!");
+}
   };
+
 
 fight(); 
