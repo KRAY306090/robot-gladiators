cbd1543685efa04ef576bac4336e11d44efcb2a8
Kennadee <kennadee.ray@gmail.com>
2020-06-17 12:35:53 -0500

    debug for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,8 @@ var enemyAttack = 12;
 var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
+    while (enemyHealth > 0) {
+
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
 
   if (promptFight === "fight" || promptFight === "FIGHT")  {
@@ -59,9 +61,12 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
 else {
     window.alert("You need to pick a valid option. Try again!");
 }
+    }
   };
 
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames [i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
